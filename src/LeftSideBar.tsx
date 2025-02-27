43c8805bdcc3d51b883a986f207d8c9f2031f70a
Patricio Vargas <patricio.vargas@cloudinary.com>
2025-02-27 02:14:02 -0500

    pulling images

--- src/LeftSideBar.tsx
+++ src/LeftSideBar.tsx
@@ -3,7 +3,7 @@ import { useTemplate } from "./TemplateContext";
 import { fill } from "@cloudinary/url-gen/actions/resize";
 
 const templates = ["1.png", "2.png", "3.png", "4.png", "5.png", "6.png"];
-const myFlyers = ["7.png", "8.png", "9.png", "10.png", "11.png", "12.png"];
+const myFlyers = ["f7.png", "f8.png", "f9.png", "f10.png", "f11.png", "f12.png"];
 
 const LeftSideBar = () => {
   const { selectedTemplate, setSelectedTemplate } = useTemplate();
@@ -14,7 +14,22 @@ const LeftSideBar = () => {
   });
 
 
-  cld.image("templates/1.png");
+  const getFlyersFromCloudinary = (images: string[], folderName: string) => {
+    return images.map((image, index: number) => {
+        const tempImg = cld.image(`${folderName}/${image}`);
+        const tempImgUrl = cld.image(`${folderName}/${image}`).toURL();
+        return (
+        <div
+          key={index}
+          className={`flex justify-center items-center relative cursor-pointer rounded-md overflow-hidden border-2 transition-all duration-300 hover:shadow-lg ${
+            selectedTemplate === `/` + image ? "border-blue-500" : "border-transparent"
+          }`}
+          onClick={() => setSelectedTemplate(tempImgUrl)}
+        >
+          <img src={tempImg.resize(fill().height(320)).toURL()} alt={`Template ${index + 1}`} className="object-cover hover:opacity-80" />
+        </div>
+      )})
+  }
 
   return (
     <div className="w-200 bg-gray-100 p-4 shadow-lg rounded-lg h-screen flex flex-col">
@@ -24,36 +39,14 @@ const LeftSideBar = () => {
       <div className="flex-1 overflow-y-auto">
         <h2 className="text-lg font-semibold mb-4 sticky top-0 bg-gray-100 p-2 z-10">Templates</h2>
         <div className="grid grid-cols-2 gap-3">
-          {templates.map((template, index) => {
-            const tempImg = cld.image(`templates/${template}`);
-            tempImg.resize(fill().height(320));
-            const tempImgUrl = cld.image(`templates/${template}`).toURL();
-            return (
-            <div
-              key={index}
-              className={`flex justify-center items-center relative cursor-pointer rounded-md overflow-hidden border-2 transition-all duration-300 hover:shadow-lg ${
-                selectedTemplate === `/` + template ? "border-blue-500" : "border-transparent"
-              }`}
-              onClick={() => setSelectedTemplate(tempImgUrl)}
-            >
-              <img src={tempImg.toURL()} alt={`Template ${index + 1}`} className="object-cover hover:opacity-80" />
-            </div>
-          )})}
+          {getFlyersFromCloudinary(templates, "templates")}
         </div>
       </div>
       <div className="border-t border-gray-300 my-2"></div>
       <div className="flex-1 overflow-y-auto">
         <h2 className="text-lg font-semibold mb-4 sticky top-0 bg-gray-100 p-2 z-10">My Flyers</h2>
         <div className="grid grid-cols-2 gap-3">
-          {myFlyers.map((flyer, index) => (
-            <div
-              key={index}
-              className="relative cursor-pointer rounded-md overflow-hidden border-2 transition-all duration-300 hover:shadow-lg border-transparent"
-              onClick={() => setSelectedTemplate(`/${flyer}`)}
-            >
-              <img src={`/${flyer}`} alt={`Flyer ${index + 1}`} className="w-full h-80 object-cover hover:opacity-80" />
-            </div>
-          ))}
+          {getFlyersFromCloudinary(myFlyers, "flyers")}
         </div>
       </div>
       <button className="bg-blue-600 text-white hover:bg-blue-200 hover:text-black cursor-pointer h-[40px] rounded-lg">
